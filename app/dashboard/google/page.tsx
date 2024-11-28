c1ccf2049d38d032b0e16719f7c14e75ab8fd7e3
Jahongir0126 <tursunboyevjahongir04@mail.ru>
2024-11-28 09:43:45 +0500

    shadcn component library

--- app/dashboard/google/page.tsx
+++ app/dashboard/google/page.tsx
@@ -1,3 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
-    return <p>Google Page</p>;
-  }
+  return <>
+  <h1>Google page</h1>
+  <div>
+      <Button className="bg-blue-600">Click me</Button>
+    </div>
+  
+  </>;
+}
