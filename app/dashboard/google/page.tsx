84c7f22d57abcdaacfb8ec38a79e49602fc79efa
Jahongir0126 <tursunboyevjahongir04@mail.ru>
2024-12-11 10:52:28 +0500

    metadating

--- app/dashboard/google/page.tsx
+++ app/dashboard/google/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Google',
+};
 export default function Page() {
     return <p>Google Page</p>;
   }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,36 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-US',
+      'uz-UZ': '/uz-UZ',
+    },
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  icons:'favicon.ico',
+  generator: 'Next.js',
+  applicationName: 'Project in Next.js',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Next.js', 'React', 'JavaScript','Jahongir','Jonidev','Jahongir0126','Desktopdev','Jakarta in Next.js'],
+  authors: [{ name: 'Jonidev' }, { name: 'Jahongir', url: 'https://nextjs.org' }],
+  creator: 'Jonidev',
+  publisher: 'Jahongir Jonidev',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+};
+
  
 export default function RootLayout({
   children,
