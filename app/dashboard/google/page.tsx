3ee0b391c0f7396f6622d6e8473f304a6454f195
Jahongir0126 <tursunboyevjahongir04@mail.ru>
2024-12-11 10:16:10 +0500

    11.12 login ended

--- app/dashboard/google/page.tsx
+++ app/dashboard/google/page.tsx
@@ -1,11 +1,3 @@
-import { Button } from "@/components/ui/button";
-
 export default function Page() {
-  return <>
-  <h1>Google page</h1>
-  <div>
-      <Button className="bg-blue-600">Click me</Button>
-    </div>
-  
-  </>;
-}
+    return <p>Google Page</p>;
+  }
